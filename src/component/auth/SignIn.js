b36ac9e3454892e778dc1e3dd4531c8641c77ebb
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-15 15:55:10 +0100

    styling buttons & inputs

--- src/component/auth/SignIn.js
+++ src/component/auth/SignIn.js
@@ -1,22 +1,23 @@
 import React from 'react'
-
+import Email from '../../assets/svg/Email'
 const Login = () => {
     return (
-        <div className='flex justify-center flex-row'>   
-            <div className='flex flex-col'>
-                <h1>welcom back</h1>
+        <div className='flex content-center justify-center'>   
+            <div className='flex flex-col bg-white rounded px-10 py-10 shadow-lg'>
+                <h1>welcome back</h1>
                 <h6>continue where you left off</h6>
                     <div className=''>
                         <button className=''>google</button>
                         <button>facebook</button>
                     </div>
                     <div className=''>
-                        <input placeholder='Email' className='px-10 py-2 my-2 w-full'/>
-                        <input placeholder='password' className='px-10 py-2 my-2  w-full'/>
+                        <Email/>
+                        <input placeholder='Email' className='border-solid border-2 border-slate-300 px-2 py-3 my-2 w-full rounded'/>
+                        <input placeholder='password' className='border-solid border-2 border-slate-300 px-2 py-3 my-2  w-full rounded'/>
                     </div>
-                    <div className=''>
-                        <button>Signin</button>
-                        <button>signup</button>
+                    <div className='flex justify-between'>
+                        <button className='border-solid border-2 px-16 py-3 rounded border-cyan-500 '>Signin</button>
+                        <button className='border-solid border-2 px-16 py-3 rounded border-cyan-500 bg-cyan-500'>signup</button>
                     </div>
             </div>
         </div>
