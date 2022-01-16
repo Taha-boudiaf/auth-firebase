b47341658b2b8bf7aea360cce3850e3ae42e7627
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-16 19:59:46 +0100

    create loading page & fix probleme router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {  SigninPage,HomePage } from './component/AsyncList';
+import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
+// import {  SigninPage } from './component/AsyncList';
+import Signin from './component/auth/Signin'
+import Home from './component/pages/Home'
 import './App.css';
 function App() {
   return (
-    <div className="container mx-auto">
-      <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route exact path='/signin' component={SigninPage}/>
-        </Switch>
-      </Router>
-    </div>
+    <>
+      <div className="container mx-auto">
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='/signin' element={<Signin/>}/>  
+          </Routes>    
+        </Router>
+      </div>
+    </>
+    
   );
 }
 
