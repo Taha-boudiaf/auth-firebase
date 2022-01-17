ad780fc41d4e9babb0eff2ae14e281457d6a7d52
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-17 23:47:45 +0100

    styling button & add icon

--- src/component/auth/SignIn.js
+++ src/component/auth/SignIn.js
@@ -7,26 +7,32 @@ const Login = () => {
             <form className='flex flex-col bg-white rounded px-10 py-10 shadow-lg content-center'>
                 <h1>welcome back</h1>
                 <h6>continue where you left off</h6>
-                    <div className=''>
-                        <button className='border-solid border px-8 py-3 rounded border-cyan-500 w-full mb-4'>Sign in with Google</button>
-                        <button className='border-solid border px-8 py-3 rounded border-cyan-500 w-full mb-4'>Sign in with Facebook</button>
+                    <div className='mt-6'>
+                        <button className='border-solid border px-8 py-3 rounded border-cyan-500 w-full mb-4'>
+                            <i class="fab fa-google text-red-500"></i>
+                            Sign in with Google
+                        </button>
+                        <button className='border-solid border px-8 py-3 rounded border-cyan-500 w-full mb-4'>
+                            <i class="fab fa-facebook text-sky-500 border-sky-500"></i>
+                            Sign in with Facebook
+                        </button>
                     </div>
-                    <div className=''>
-                        <label>Email</label>
+                    <div className='mt-6'>
+                        <label className='mt-4'>Email</label>
                         <div className='relative'>
                             <i className="fas fa-at absolute h-full w-12 justify-center flex items-center text-gray-400 "></i>
                             <input type="email" placeholder='example@email.com' className='border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
                         </div>
-                        <label>Password</label>
+                        <label className='mt-4'>Password</label>
                         <div className='relative'>
                             <i className="fas fa-fingerprint absolute h-full w-12 justify-center flex items-center text-gray-400 "></i>
                             <input type="password" placeholder='********' className='border-solid border shadow-sm border-slate-300 px-4 py-4 my-2  w-full rounded pl-10'/>
                         </div> 
-                        <span>forget your <Link to="#"><u>password ?</u></Link> </span>                      
+                        <span className='text-gray-300 flex justify-end'>forget your <Link to="#"><u className='hover:text-blue-600'> password ?</u></Link> </span>                      
                     </div>
                     <div className='flex justify-between'>
-                        <button className='border-solid border px-16 py-3 rounded border-lime-500  shadow-sm'>Signin</button>
-                        <button className='border-solid border px-16 py-3 rounded border-lime-500 bg-lime-500 shadow-sm'>signup</button>
+                        <button className='border-solid border mt-6 px-16 py-3 rounded border-blue-600  shadow-sm text-blue-600'>Signin</button>
+                        <button className='border-solid border mt-6 px-16 py-3 rounded bg-blue-600 shadow-sm text-white'>signup</button>
                     </div>
             </form>
         </div>
