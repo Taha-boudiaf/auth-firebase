ad8e42f051a9a2babca6e3b3c5d90193b4ca88b0
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-20 14:06:52 +0100

    use comment to explain function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,29 +3,32 @@ import {auth} from "../firebase"
 
 
 const AuthContext = createContext()
+// export context 
 export const useAuth = ()=>{
 const context = useContext(AuthContext)
 }
+
+
 export const AuthProvider = ({children}) => {
+    // state to save users 
     const [currentUser,setCurentUser] = useState()
+    // to create user with email and password 
     const Signup =(email,password) =>{
         return auth.createUserWithEmailandPassword(email,password);
     }
-
+    // if user signup move data to state <<currentUser>>
     useEffect(() => {
         const Users = auth.onAuthStateChanged((user)=>{
             setCurentUser(user)
         })
         return Users
     }, [])
-
-   
-
+    // props value to pass data to children 
     const value = {
         currentUser,
         Signup
     }
-
+    // auth provider 
     return (
         <AuthContext.Provider value={value}>
             {children}
