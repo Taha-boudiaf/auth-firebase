4cb922cbbd62bc43191b0bea565b64641f8de3bc
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-22 12:58:20 +0100

    integration signin and signup

--- src/component/auth/SignIn.js
+++ src/component/auth/SignIn.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react/cjs/react.development'
+import { useAuth } from '../../context/AuthContext'
 
 const Login = () => {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [error,setError] = useState('')
+    const {Signin} =  useAuth();
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        setError('')
+        try {
+           await Signin(email,password);
+
+        } catch (error) {
+            setError(error.message)
+        }
+    }
     return (
         <div className='flex justify-center mt-16 mb-16'>   
             <form className='flex flex-col bg-white rounded px-10 py-10 shadow-lg content-center'>
@@ -26,20 +41,20 @@ const Login = () => {
                             <label className='text-gray-600 font-semibold'>Email</label>
                             <div className='relative'>
                                 <i className="fas fa-at absolute h-full w-12 justify-center flex items-center text-gray-400 "></i>
-                                <input type="email" placeholder='example@email.com' className='focus:border-blue-500 focus:ring-1 focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
+                                <input type="email" value={email} onChange={(e)=>e.target.value} placeholder='example@email.com' className='focus:border-blue-500 focus:ring-1 focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
                             </div>
                         </div>
                         <div className='mt-4'>
                             <label className='text-gray-600 font-semibold'>Password</label>
                             <div className='relative'>
                                 <i className="fas fa-fingerprint absolute h-full w-12 justify-center flex items-center text-gray-400 "></i>
-                                <input type="password" placeholder='********' className='focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2  w-full rounded pl-10'/>
+                                <input type="password" value={password} onChange={(e)=> e.target.value} placeholder='********' className='focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2  w-full rounded pl-10'/>
                             </div> 
                         </div>
                         <span className='text-gray-400 flex justify-end'>Forget your <a href='#' className='ml-1 text-blue-600 underline underline-offset-2'> Password ?</a> </span>                      
                     </div>
                     <div className='mt-6 flex justify-between'>
-                        <button type='submit' className='font-semibold border-solid border mt-6 px-16 py-3 rounded border-blue-600 bg-blue-600  shadow-sm text-white w-full'>Sign in</button>
+                        <button type='submit' onClick={handleSubmit} className='font-semibold border-solid border mt-6 px-16 py-3 rounded border-blue-600 bg-blue-600  shadow-sm text-white w-full'>Sign in</button>
                     </div>
                     <p className='mt-6 text-center font-semibold'>don't have an account ? <Link to="/signup" className='underline underline-offset-2 text-blue-600'>Sign Up</Link> </p>
             </form>
--- src/component/auth/Signup.js
+++ src/component/auth/Signup.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import { useState } from 'react/cjs/react.development'
 import { useAuth } from '../../context/AuthContext'
 
 
 const Signup = () => {
+    // data 
+    const [email,setEmail] = useState('');
+    const [userName,setUserName] = useState('');
+    const [password,setPassword] = useState();
+    const {Signup} = useAuth();
+    const[error,setError] = useState('')
+    const Navigate = useNavigate('')
+    // submit function 
+    const handleSubmit = async (e)=> {
+        e.preventDefault();
+        setError('')
+       try {
+           await Signup(email,password)
+           Navigate("/");
+       } catch (err) {
+           setError(err.message);
+       } 
+    }
     return (
         <div className='flex justify-center mt-16 mb-16'>   
+        
             <form className='flex flex-col bg-white rounded px-10 py-10 shadow-lg content-center'>
+            
                 <h1 className='text-5xl font-bold mb-6 text-center'>Sign Up</h1>
                     <div className='relative mt-6 flex justify-between'>
                         <button className='font-semibold shadow-sm border-solid border px-8 py-3 rounded border-red-500  mb-4 hover:bg-red-500 hover:text-white text-center w-1/2 mr-1'>
@@ -27,26 +48,27 @@ const Signup = () => {
                             <label className='text-gray-600 font-semibold'>Username</label>
                             <div className='relative'>
                                 <i className="far fa-user-circle absolute h-full w-12 justify-center flex items-center text-gray-400"></i>
-                                <input type="text" placeholder='Username' className='focus:border-blue-500 focus:ring-1 focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
+                                <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} placeholder='Username' className='focus:border-blue-500 focus:ring-1 focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
                             </div>
                         </div>
                         <div className='mt-4'>
                             <label className='text-gray-600 font-semibold'>Email</label>
                             <div className='relative'>
                                 <i className="fas fa-at absolute h-full w-12 justify-center flex items-center text-gray-400 "></i>
-                                <input type="email" placeholder='example@email.com' className='focus:border-blue-500 focus:ring-1 focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
+                                <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='example@email.com' className='focus:border-blue-500 focus:ring-1 focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2 w-full rounded pl-10'/>
                             </div>
                         </div>
                         <div className='mt-4'>
                             <label className='text-gray-600 font-semibold'>Password</label>
                             <div className='relative'>
                                 <i className="fas fa-fingerprint absolute h-full w-12 justify-center flex items-center text-gray-400 "></i>
-                                <input type="password" placeholder='********' className='focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2  w-full rounded pl-10'/>
+                                <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder='********' className='focus:ring-blue-600 border-solid border shadow-sm border-slate-300 px-4 py-4 my-2  w-full rounded pl-10'/>
                             </div> 
                         </div>
+                        {error &&  <div className='bg-red-300 w-full px-4 py-4 rounded mt-6'>{error} </div>}
                     </div>
                     <div className='mt-6 flex justify-between'>
-                        <button type='submit' className='font-semibold border-solid border mt-6 px-16 py-3 rounded border-blue-600 bg-blue-600  shadow-sm text-white w-full'>Sign in</button>
+                        <button type='submit' onClick={handleSubmit} className='font-semibold border-solid border mt-6 px-16 py-3 rounded border-blue-600 bg-blue-600  shadow-sm text-white w-full'>Sign in</button>
                     </div>
                     <p className='mt-6 text-center font-semibold'>do you have an account ? <Link to="/signin" className='underline underline-offset-2 text-blue-600'>Sign In</Link> </p>
             </form>
