9e2e82bfe5eee16ceda3abcc9f12e93db1219ed5
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-22 13:02:25 +0100

    add component context provider

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
 import {  SigninPage,HomePage,SignupPage } from './component/AsyncList';
 
 import './App.css';
+import { UserAuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <>
       <div className="container mx-auto">
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<HomePage/>}/>
-            <Route path='/signin' element={<SigninPage/>}/>  
-            <Route path='/signup' element={<SignupPage/>}/>  
-          </Routes>    
-        </Router>
+        <UserAuthContextProvider>
+          <Router>
+            <Routes>
+              <Route exact path='/' element={<HomePage/>}/>
+              <Route path='/signin' element={<SigninPage/>}/>  
+              <Route path='/signup' element={<SignupPage/>}/>  
+            </Routes>    
+          </Router>
+        </UserAuthContextProvider>  
       </div>
     </>
     
