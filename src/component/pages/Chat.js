878720cc5ad7b66903e0e282fdf0976b1a22c37d
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-23 15:35:29 +0100

    init chat page

--- src/component/pages/Chat.js
+++ src/component/pages/Chat.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Chat = () => {
-  return <div>hello chat</div>;
+  return ( 
+  <div>
+    <p>chat page </p>
+    <button className='px-5 py-5 bg-blue-600 text-white'>Logout</button>
+  </div>
+  )
 };
 
 export default Chat;
