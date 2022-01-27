6cc2a1464fd537f84431a009fa5816a71a8bcf56
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-27 22:32:42 +0100

    styling not found page

--- src/component/pages/PageNotFound.js
+++ src/component/pages/PageNotFound.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const PageNotFound = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center	h-screen items-center	 bg-white'>
+            <div>
+                <h1 className='text-6xl'>OOPS</h1>
+                <h1 className='text-9xl'>404</h1>
+                <p className='text-4xl'>Something Went Wrong</p>
+                <button className=''>Go Back </button>
+            </div>
         </div>
     )
 }
