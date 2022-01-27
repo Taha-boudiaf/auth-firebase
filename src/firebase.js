a2c6b1646012822002a344557b9a6cdd74f144a4
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-27 20:20:32 +0100

    private information by using env local

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp} from "firebase/app";
 import {getAuth} from 'firebase/auth'
 // configuration firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBV3C7fwqpF8wAE4RnpcB7mkVqSVBqw1cg",
-  authDomain: "let-s-chat-7f491.firebaseapp.com",
-  projectId: "let-s-chat-7f491",
-  storageBucket: "let-s-chat-7f491.appspot.com",
-  messagingSenderId: "796953381031",
-  appId: "1:796953381031:web:e1ee4efb8504b80b1fb382",
-  measurementId: "G-JTXN95V5CE"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN ,
+  projectId:process.env.REACT_APP_PROJECT_ID ,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID ,
+  appId:process.env.REACT_APP_APP_ID ,
+  measurementId:process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // init firebase app 
