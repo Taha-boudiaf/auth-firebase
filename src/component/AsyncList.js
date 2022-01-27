d3f2c151afc466ba50e4984beb13ca212e0c6c52
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-27 22:32:24 +0100

    add not found page

--- src/component/AsyncList.js
+++ src/component/AsyncList.js
@@ -5,6 +5,7 @@ const Home = lazy(() => import('./pages/Home'));
 const Signin = lazy(() => import('./auth/Signin'));
 const Signup = lazy(() => import('./auth/Signup'));
 const Chat = lazy (()=> import('./pages/Chat'))
+const PageNotFound = lazy (()=> import('./pages/PageNotFound'))
 
 export const ChatPage = () =>(
     <Suspense fallback={<Loading/>}>
@@ -27,6 +28,12 @@ export const SignupPage = () =>(
 export const HomePage = () =>(
     <Suspense fallback={<Loading/>}>
         <Home/>
+    </Suspense>
+)
+
+export const NotFoundPage = () =>(
+    <Suspense fallback={<Loading/>}>
+        <PageNotFound/>
     </Suspense>
 )
     
