c66f4bb1c8484d94ce0ce1c0e3f7fe05192794d1
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-27 22:32:08 +0100

    use hook location to locate path

--- src/component/pages/ProtectedRoute.js
+++ src/component/pages/ProtectedRoute.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate,useLocation  } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
 const ProtectedRoute = ({children,...res}) => {
     const {currentUser} =  useAuth();
-    
+    const location = useLocation();
     if (!currentUser) {
-        return <Navigate to="/signin"/>
+        return <Navigate to="/signin" state={{path:location.pathname}}/>
     }
     
     return children
