3695d4c9d743986e47f2d46c29e5f7559529a4e9
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-27 23:15:54 +0100

    styling button + back to home

--- src/component/pages/PageNotFound.js
+++ src/component/pages/PageNotFound.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const PageNotFound = () => {
     return (
-        <div className='flex justify-center	h-screen items-center	 bg-white'>
-            <div>
-                <h1 className='text-6xl'>OOPS</h1>
-                <h1 className='text-9xl'>404</h1>
-                <p className='text-4xl'>Something Went Wrong</p>
-                <button className=''>Go Back </button>
+        <div className='flex flex-col justify-center	h-screen items-center  bg-white'>
+            <div className='text-center'>
+                <h1 className='mt-4 text-6xl font-semibold '>OOPS</h1>
+                <h1 className='mt-4 text-9xl font-extrabold '>404</h1>
+                <p className='mt-4 text-4xl font-semibold '>Something Went Wrong</p>
             </div>
+            <a href='/' className='mt-8 font-normal px-8 py-4 bg-blue-600 rounded shadow-blue-600/50 text-white'>Back To Home </a>
         </div>
     )
 }
