f5e71e5a79b930e629425936b15f2383ab282398
Taher Boudiaf <husseintaha168@gmail.com>
2022-01-27 22:34:32 +0100

    add footer & navbar to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
-import {  SigninPage,HomePage,SignupPage,ChatPage } from './component/AsyncList';
+import {  SigninPage,HomePage,SignupPage,ChatPage,NotFoundPage } from './component/AsyncList';
 import { UserAuthContextProvider } from './context/AuthContext';
 import ProtectedRoute from './component/pages/ProtectedRoute';
-
+import Footer from './component/layout/Footer'
+import Navbar from './component/layout/Navbar'
 import './App.css';
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <div className="container mx-auto">
+        <Navbar/>
         <UserAuthContextProvider>
           <Router>
             <Routes>
@@ -18,9 +20,11 @@ function App() {
               <Route path='/chat' element={<ProtectedRoute><ChatPage/></ProtectedRoute>} />
               <Route path='/signin' element={<SigninPage/>}/>  
               <Route path='/signup' element={<SignupPage/>}/>  
+              <Route path='*' element={<NotFoundPage/>}/>
             </Routes>    
           </Router>
         </UserAuthContextProvider>  
+        <Footer/>
       </div>
     </>
     
