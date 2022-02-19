e5bc11cc21c15b876822adc9d90e807ea7ccfb30
Taher Boudiaf <husseintaha168@gmail.com>
2022-02-19 12:46:43 +0100

    design home page

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -10,10 +10,13 @@ const Home = () => {
     }
   }
   return ( 
-  <div>
-    <p>Home page </p>
-    <button className='px-5 py-5 bg-blue-600 text-white' onClick={handellogout}>Logout</button>
-  </div>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex justify-center	mt-28 space-x-4"> 
+      <div className='text-center'>
+          <div class="text-xl font-medium text-black">Home Page</div>
+          <p class="text-slate-500 my-5">Click button to log out</p>
+          <button className='px-8 py-2 bg-blue-600 text-white rounded' onClick={handellogout}>Logout</button>
+      </div>
+    </div>
   )
 };
 
